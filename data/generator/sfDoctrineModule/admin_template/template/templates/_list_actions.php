50d962186d6fba9fea072b7e7d07cb973f88e281
Vincent CHALAMON <vincentchalamon@gmail.com>
2012-07-27 21:02:06 +0200

    update list actions generator

--- data/generator/sfDoctrineModule/admin_template/template/templates/_list_actions.php
+++ data/generator/sfDoctrineModule/admin_template/template/templates/_list_actions.php
@@ -5,7 +5,13 @@
 <?php echo $this->addCredentialCondition('[?php echo $helper->linkToNew('.$this->asPhp($params).') ?]', $params)."\n" ?>
 <?php else: ?>
 <li class="sf_admin_action_<?php echo $params['class_suffix'] ?>">
+[?php if (method_exists($helper, 'linkTo<?php echo $method = ucfirst(sfInflector::classify(preg_replace('/^_(.*)/i', '$1', $name))) ?>')): ?]
+  <?php echo $this->addCredentialCondition('[?php echo $helper->linkTo'.$method.'('.$this->asPhp($params).') ?]', $params) ?>
+
+[?php else: ?]
   <?php echo $this->addCredentialCondition($this->getLinkToAction($name, $params, false), $params)."\n" ?>
+
+[?php endif; ?]
 </li>
 <?php endif; ?>
 <?php endforeach; ?>
